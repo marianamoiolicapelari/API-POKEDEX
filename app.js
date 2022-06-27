967a0dba99c765da6ce3eb4fd8a57eee3d0cd157
marianamoiolicapelari <marianamoioli@gmail.com>
2022-06-27 09:53:24 -0300

    Added routes

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
+const rotaProdutos = require('./routes/produtos')
+const rotaPedidos = require('./routes/pedidos')
+
+app.use(morgan('dev'))
+
+app.use('/produtos', rotaProdutos)
+app.use('/pedidos', rotaPedidos)
+
+//QUANDO NÃO ENCONTRA ROTA ENTRA AQUI
 app.use((req, res, next) => {
-    res.status(200).send({
-      mensagem: 'OK, Deu certo'
-    })
+  const erro = new Error('Não encontrado')
+  erro.status = 404
+  next(erro)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  return res.send({
+    erro: {
+      mensagem: error.message
+    }
+  })
 })
 
 module.exports = app
